bea3b4a67d4864d9f744684b1ff31947259ca629
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-05 21:57:04 -0300

    refact: update single page

--- resources/views/show_produto.blade.php
+++ resources/views/show_produto.blade.php
@@ -8,10 +8,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     @vite('resources/css/app.css')
+    @vite('resources/css/show-prod.css')
     <title>{{ $produto ? $produto->nome : 'Detalhes do Produto' }}</title>
 </head>
 <body>
-    @vite('resources/css/show-prod.css')
     <div class="container text-center">
         <h1>{{ $produto ? $produto->nome : 'Detalhes do Produto' }}</h1>
         @if ($produto)
