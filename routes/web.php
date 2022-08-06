47e26fcb89e9fe3b09cc7120c11feb6396e41c66
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-05 21:57:20 -0300

    feat: created route to crud pages

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,43 @@ Route::get('/ola',function(){
 Route::get('/aloControl',
     [HomeController::class,'index']);
 
-Route::get('/produto',
+Route::get('/produtos',
  [ProdutoController::class,'index']);
 
 Route::get('/produto/{id}',[
-    ProdutoController::class,'show']);
+    ProdutoController::class,
+    'show'
+])->name('show');
+
+Route::get('/produto',[
+    ProdutoController::class,
+    'create'
+]);
+
+Route::post('/produto',[
+    ProdutoController::class,
+    'store'
+]);
+
+Route::get('/produto/{id}/edit',[
+    ProdutoController::class,
+    'edit'
+])->name('edit');
+
+Route::post('/produto/{id}/update',[
+    ProdutoController::class,
+    'update'
+])->name('update');
+
+Route::get('/produto/{id}/delete',[
+    ProdutoController::class,
+    'delete'
+])->name('delete');
+
+Route::post('/produto/{id}/remove',[
+    ProdutoController::class,
+    'remove'
+])->name('remove');
+
+
+
