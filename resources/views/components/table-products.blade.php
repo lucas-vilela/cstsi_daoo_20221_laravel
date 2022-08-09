e26fe38bd1abd3d3f8e4172779dd5f3cc9902b1a
G1ll D3567 W10 <gillvelleda@gmail.com>
2022-08-09 17:36:54 -0300

    refact: create component for table products

---
+++ resources/views/components/table-products.blade.php
@@ -0,0 +1,15 @@
+
+<table class='table  table-striped'>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th>Importado</th>
+        </tr>
+    </thead>
+        <tbody>
+            {{$slot}}
+        </tbody>
+</table>
