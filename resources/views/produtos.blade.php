8c566be16b0e52871be1319dfe9856bee68aa239
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-11 19:54:46 -0300

    refact: using new breeze layout

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,14 +1,20 @@
-<x-layouts.app>
-    <h1>Produtos</h1>
+<x-app-layout>
+    <x-slot name="header">
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Produtos') }}
+        </h2> --}}
+        <h1 class='text-4xl'>Produtos no Estoque</h1>
+    </x-slot>
+
     <div class="container text-center">
         <div class="row align-items-center">
             <div class="col">
                 @if ($produtos->count() > 0)
-                    <x-table.products :products="$produtos" type="hover"/>
+                    <x-products-table :products="$produtos" type="hover"/>
                 @else
                     <p>Produtos não encontrados! </p>
                 @endif
             </div>
         </div>
     </div>
-</x-layouts.app>
+</x-app-layout>
