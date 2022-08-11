c71ff31abde35f1eaa8bf4575c40aa5f1cfcc039
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-11 16:19:33 -0300

    fix: added products routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,49 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function(){
+    Route::get('/produtos',
+    [ProdutoController::class,'index']);
+
+   Route::get('/produto/{id}',[
+       ProdutoController::class,
+       'show'
+   ])->name('show');
+
+   Route::get('/produto',[
+       ProdutoController::class,
+       'create'
+   ]);
+
+   Route::post('/produto',[
+       ProdutoController::class,
+       'store'
+   ]);
+
+   Route::get('/produto/{id}/edit',[
+       ProdutoController::class,
+       'edit'
+   ])->name('edit');
+
+   Route::post('/produto/{id}/update',[
+       ProdutoController::class,
+       'update'
+   ])->name('update');
+
+   Route::get('/produto/{id}/delete',[
+       ProdutoController::class,
+       'delete'
+   ])->name('delete');
+
+   Route::post('/produto/{id}/remove',[
+       ProdutoController::class,
+       'remove'
+   ])->name('remove');
+});
+
+
+
+
 
 require __DIR__.'/auth.php';
