d2efdfaa16c69681253c66aade1fb99d5d7ebfbb
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-12 14:04:54 -0300

    refacr: indentetion

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -6,27 +6,27 @@
                 <div class="col">
                     <x-table-products>
                         @foreach ($produtos as $produto)
-                        <tr>
-                            <td><a href="{{ route('show', $produto->id) }}">
-                                    {{ $produto->id }}
-                                </a>
-                            </td>
-                            <td>{{ $produto->nome }}</td>
-                            <td>{{ $produto->qtd_estoque }}</td>
-                            <td>{{ $produto->preco }}</td>
-                            <td>{{ $produto->importado ? 'Sim' : 'Não' }}</td>
-                            <td>
-                                <a href="{{ route('edit', $produto->id) }}">
-                                    <button class='btn btn-primary btn-sm'>Editar</button>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{ route('delete', $produto->id) }}">
-                                    <button class='btn btn-danger btn-sm'>Remover</button>
-                                </a>
-                            </td>
-                        </tr>
-                    @endforeach
+                            <tr>
+                                <td><a href="{{ route('show', $produto->id) }}">
+                                        {{ $produto->id }}
+                                    </a>
+                                </td>
+                                <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->qtd_estoque }}</td>
+                                <td>{{ $produto->preco }}</td>
+                                <td>{{ $produto->importado ? 'Sim' : 'Não' }}</td>
+                                <td>
+                                    <a href="{{ route('edit', $produto->id) }}">
+                                        <button class='btn btn-primary btn-sm'>Editar</button>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ route('delete', $produto->id) }}">
+                                        <button class='btn btn-danger btn-sm'>Remover</button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
                     </x-table-products>
                 </div>
             </div>
