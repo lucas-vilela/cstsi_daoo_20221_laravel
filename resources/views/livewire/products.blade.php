3ebb8db41f9da94531f9d4ee52bf5f7e9b90dcc9
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-26 16:40:22 -0300

    feat: insert table into container

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,11 +1,9 @@
-<div class="container text-center">
-    <div class="row align-items-center">
-        <div class="col">
-            @if ($produtos->count() > 0)
-                <x-products-table :listProducts="$produtos" type="striped"/>
-            @else
-                <p>Produtos não encontrados! </p>
-            @endif
-        </div>
+<div class="flex flex-col w-full h-full">
+    <div  class="flex flex-col justify-center w-5/6 shadow bg-white dark:bg-gray-700 h-auto mt-3 p-2 self-center rounded-md">
+        @if ($produtos->count() > 0)
+            <x-products-table :listProducts="$produtos" type="striped" />
+        @else
+            <p>Produtos não encontrados! </p>
+        @endif
     </div>
 </div>
