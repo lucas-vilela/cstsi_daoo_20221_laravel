971bdea0ee3195ce04bd15722db7c148db7db033
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-26 16:38:43 -0300

    refact: refactoring code

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,8 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('produtos')" :active="request()->routeIs('produtos')">
+                    <x-nav-link :href="route('produtos')"
+                        :active="request()->routeIs('produtos')">
                         {{ __('Produtos') }}
                     </x-nav-link>
                 </div>
