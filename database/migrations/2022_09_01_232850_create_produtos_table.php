58a9d80817f2ad47a85523ed4f748f06ac9d9c1f
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-01 20:46:03 -0300

    feat: criacao da migration de Produto

--- database/migrations/2022_09_01_232850_create_produtos_table.php
+++ database/migrations/2022_09_01_232850_create_produtos_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->text('nome');
-            $table->text('descricao');
-            $table->integer('qtd_estoque');
-            $table->string('codigo_barra')->default('000');
+            $table->foreignId('fornecedor_id')
+                    ->references('id')->on('fornecedores')
+                    ->cascadeOnDelete();
+            $table->string('nome');
+            $table->string('descricao');
             $table->float('preco');
+            $table->integer('qtd_estoque');
             $table->boolean('importado')->default(false);
             $table->timestamps();
         });
