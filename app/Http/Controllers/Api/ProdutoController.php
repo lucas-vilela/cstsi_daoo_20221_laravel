0e90b5b67cd33c93f0d811eae3386e5a51cc37f7
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-08 17:27:36 -0300

    feat: api rest controlador para Produto. php artisan make:controller Api/ProdutoController

---
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Produto::all());
+    }
+}
