6ceb3f9e4585af367475ee6255b60b18f5aedf63
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-08 19:15:17 -0300

    feat: criada a rota post para store em produto

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('produto',[ProdutoController::class,'index']);
 Route::get('produto/{id}',[ProdutoController::class,'show']);
+Route::post('produto',[ProdutoController::class,'store']);
