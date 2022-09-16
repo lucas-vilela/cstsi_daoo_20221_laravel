54d87f9508185b69842fd5eb4eb05e7bca92874b
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-16 16:50:02 -0300

    feat: criada a model para fotos

--- app/Models/Fotos.php
+++ app/Models/Fotos.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Fotos extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'url',
+        'filename',
+        'id_produto'
+    ];
+
+    public function fotos(){
+        return $this->belongsTo(Produto::class);
+    }
 }
