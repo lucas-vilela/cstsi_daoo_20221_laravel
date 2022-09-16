af1dc171a7b2733f82629b783bc986e2dbd77370
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-16 16:36:27 -0300

    feat: criacao logout

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
@@ -27,5 +28,17 @@ class LoginController extends Controller
         }
     }
 
+    public function logout(Request $request)
+    {
+        $auth_user = $request->user();
+        if($request->input('all')){
+            if($auth_user->tokens()->delete())
+                return ['logout'=>'Desconectado de todos os dispositivos!'];
+        }else{
+            if($auth_user->currentAccesstoken()->delete())
+                return ['logout'=>'Usuário desconectado!'];
+        }
+        return response()->json(['logout'=>'Falha ao desconectar Usuário!'],500);
+    }
 
 }
