b5920eeeb8056bdd3745595e67d7fe22814a8fc9
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-21 16:46:28 -0300

    refact: formatacao do primeira rota

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/token', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/token',
+    function (Request $request) {
     return $request->user();
 });
 
